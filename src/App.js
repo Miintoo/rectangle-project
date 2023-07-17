32896656725fae903841e5a0dae66949dd994852
Minto <xpeed96@gmail.com>
2023-07-17 18:49:47 +0900

    Add: 프로젝트 제출

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="rectangle_box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className='out_box_first'></div>
+      <div className='out_box_second'></div>
+      <div className='out_box_third'></div>
+      <div className='out_box_firth'></div>
+    </>
   );
 }
 
